9f37bea2ae9f2920ada465c7f0b0421389d0c669
Lensom <magurawork@gmail.com>
2020-04-23 16:54:31 +0300

    End db work

--- routes/orders.js
+++ routes/orders.js
@@ -1,20 +1,35 @@
-const { Router } = require('express');
-const Order = require('../models/order');
-const router = Router();
+const { Router } = require('express')
+const Order = require('../models/order')
+const router = Router()
 
 router.get('/', async (req, res) => {
-  res.render('orders', {
-    isOrder: true,
-    title: 'Заказы'
-  })
+  try {
+    const orders = await Order.find({ 'user.userId': req.user._id })
+      .populate('user.userId')
+
+    res.render('orders', {
+      isOrder: true,
+      title: 'Заказы',
+      orders: orders.map(o => {
+        return {
+          ...o._doc,
+          price: o.courses.reduce((total, c) => {
+            return total += c.count * c.course.price
+          }, 0)
+        }
+      })
+    })
+  } catch (e) {
+    console.log(e)
+  }
 })
 
-router.post('/', async (res, req) => {
 
+router.post('/', async (req, res) => {
   try {
     const user = await req.user
       .populate('cart.items.courseId')
-      .execPopulate();
+      .execPopulate()
 
     const courses = user.cart.items.map(i => ({
       count: i.count,
@@ -26,17 +41,16 @@ router.post('/', async (res, req) => {
         name: req.user.name,
         userId: req.user
       },
-      courses
+      courses: courses
     })
 
-    await order.save();
-    await req.user.clearCart();
+    await order.save()
+    await req.user.clearCart()
 
-
-    res.redirect('/orders');
+    res.redirect('/orders')
   } catch (e) {
     console.log(e)
   }
 })
 
-module.exports = router;
+module.exports = router
